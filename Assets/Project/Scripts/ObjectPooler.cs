c3c9ffc49942b163aedde4af633f1a360e96bf56
Justin Wong <jusw85@hotmail.com>
2019-04-28 14:26:46 +0800

    Add Object Pooler

---
+++ Assets/Project/Scripts/ObjectPooler.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooler : MonoBehaviour
+{
+    public static ObjectPooler instance;
+
+    private Dictionary<int, Queue<GameObject>> pools;
+
+    private List<GameObject> pooledObjects;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        pools = new Dictionary<int, Queue<GameObject>>();
+    }
+
+    public bool CreatePool(GameObject obj, int size)
+    {
+        int id = obj.GetInstanceID();
+        if (pools.ContainsKey(id))
+        {
+            return false;
+        }
+
+        Queue<GameObject> queue = new Queue<GameObject>();
+        for (int i = 0; i < size; i++)
+        {
+            var go = Instantiate(obj);
+            go.SetActive(false);
+            queue.Enqueue(go);
+        }
+
+        pools.Add(id, queue);
+        return true;
+    }
+
+    public GameObject GetPoolObject(GameObject obj)
+    {
+        int id = obj.GetInstanceID();
+        Queue<GameObject> queue;
+        pools.TryGetValue(id, out queue);
+        if (queue == null)
+        {
+            return null;
+        }
+
+        var res = queue.Dequeue();
+        queue.Enqueue(res);
+        return res;
+    }
+}
---
+++ Assets/Project/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject slave;
+    public int numSlaves;
+
+    private void Start()
+    {
+        var objectPooler = ObjectPooler.instance;
+        objectPooler.CreatePool(slave, Mathf.Max(50, numSlaves));
+        for (int i = 0; i < numSlaves; i++)
+        {
+            var obj = objectPooler.GetPoolObject(slave);
+            obj.SetActive(true);
+            obj.transform.position = transform.position;
+            obj.transform.rotation = Quaternion.identity;
+        }
+    }
+}
