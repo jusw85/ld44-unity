5c0575ee50b01b95169b5561685d340b3e41c9ff
Justin Wong <jusw85@hotmail.com>
2019-04-28 14:43:18 +0800

    Add random spawner

--- Assets/Project/Scripts/PlayerController.cs
+++ Assets/Project/Scripts/PlayerController.cs
@@ -4,17 +4,37 @@ public class PlayerController : MonoBehaviour
 {
     public GameObject slave;
     public int numSlaves;
+    public GameObject spawnArea;
+
+    private ObjectPooler objectPooler;
 
     private void Start()
     {
-        var objectPooler = ObjectPooler.instance;
+        objectPooler = ObjectPooler.instance;
         objectPooler.CreatePool(slave, Mathf.Max(50, numSlaves));
+
+        BoxCollider2D[] colliders = spawnArea.GetComponentsInChildren<BoxCollider2D>();
+
         for (int i = 0; i < numSlaves; i++)
         {
             var obj = objectPooler.GetPoolObject(slave);
             obj.SetActive(true);
-            obj.transform.position = transform.position;
+            var bounds = colliders[Random.Range(0, colliders.Length)].bounds;
+            obj.transform.position = RandomPointInBounds(bounds);
             obj.transform.rotation = Quaternion.identity;
         }
+    }
+
+    private void SpawnSlave()
+    {
+    }
+
+    private static Vector3 RandomPointInBounds(Bounds bounds)
+    {
+        return new Vector3(
+            Random.Range(bounds.min.x, bounds.max.x),
+            Random.Range(bounds.min.y, bounds.max.y),
+            Random.Range(bounds.min.z, bounds.max.z)
+        );
     }
 }
