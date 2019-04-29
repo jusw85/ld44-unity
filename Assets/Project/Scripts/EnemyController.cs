df218ce825c8811e2b37ac8d4e556e53d4bc6680
Justin Wong <jusw85@hotmail.com>
2019-04-29 15:26:30 +0800

    Add enemy slave spawn

--- Assets/Project/Scripts/EnemyController.cs
+++ Assets/Project/Scripts/EnemyController.cs
@@ -4,22 +4,11 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour, IHasHP
 {
     public int hp;
-
-    public void TakeDamage(int dmg)
-    {
-        hp -= dmg;
-    }
-
-    public int GetHP()
-    {
-        return hp;
-    }
-
     public GameObject slave;
     public int initialSlaves;
     public GameObject spawnArea;
     public GameObject fireball;
-    public EnemyController enemy;
+    public PlayerController player;
 
     private Animator animator;
     private ObjectPooler objectPooler;
@@ -46,8 +35,9 @@ public class EnemyController : MonoBehaviour, IHasHP
             spawnBounds[i] = colliders[i].bounds;
         }
 
-//        SpawnSlaves(initialSlaves);
+        SpawnSlaves(initialSlaves);
     }
+
 //
 //    public void HandleSkill(String key, int cost)
 //    {
@@ -98,28 +88,30 @@ public class EnemyController : MonoBehaviour, IHasHP
 //            });
 //    }
 //
-//    public void SpawnSlaves(int numSlaves)
-//    {
-//        for (int i = 0; i < numSlaves; i++)
-//        {
-//            var obj = objectPooler.GetPoolObject(slave);
-//            obj.SetActive(true);
-//            var bounds = spawnBounds[Random.Range(0, spawnBounds.Length)];
-//            var xy = RandomPointInBounds(bounds);
-//            xy.z = 0;
-//            obj.transform.position = xy;
-//            obj.transform.rotation = Quaternion.identity;
-//
-//            Animator anim = obj.GetComponent<Animator>();
-//            for (int j = 0; j < anim.layerCount; j++)
-//            {
-//                anim.SetLayerWeight(j, 0f);
-//            }
-//
-//            anim.SetLayerWeight(Random.Range(0, anim.layerCount), 1f);
-//            slaveObjs.Enqueue(obj);
-//        }
-//    }
+    public void SpawnSlaves(int numSlaves)
+    {
+        for (int i = 0; i < numSlaves; i++)
+        {
+            var obj = objectPooler.GetPoolObject(slave);
+            obj.SetActive(true);
+            obj.GetComponent<SpriteRenderer>().flipX = true;
+            var bounds = spawnBounds[Random.Range(0, spawnBounds.Length)];
+            var xy = RandomPointInBounds(bounds);
+            xy.z = 0;
+            obj.transform.position = xy;
+            obj.transform.rotation = Quaternion.identity;
+
+            Animator anim = obj.GetComponent<Animator>();
+            for (int j = 0; j < anim.layerCount; j++)
+            {
+                anim.SetLayerWeight(j, 0f);
+            }
+
+            anim.SetLayerWeight(Random.Range(3, anim.layerCount), 1f);
+            slaveObjs.Enqueue(obj);
+        }
+    }
+
 //
 //    public void SacSlaves(int numSlaves)
 //    {
@@ -136,17 +128,22 @@ public class EnemyController : MonoBehaviour, IHasHP
 //        return slaveObjs.Count;
 //    }
 //
-//    private static Vector3 RandomPointInBounds(Bounds bounds)
-//    {
-//        return new Vector3(
-//            Random.Range(bounds.min.x, bounds.max.x),
-//            Random.Range(bounds.min.y, bounds.max.y),
-//            Random.Range(bounds.min.z, bounds.max.z)
-//        );
-//    }
-//
-//    public int GetHP()
-//    {
-//        return hp;
-//    }
+    private static Vector3 RandomPointInBounds(Bounds bounds)
+    {
+        return new Vector3(
+            Random.Range(bounds.min.x, bounds.max.x),
+            Random.Range(bounds.min.y, bounds.max.y),
+            Random.Range(bounds.min.z, bounds.max.z)
+        );
+    }
+
+    public void TakeDamage(int dmg)
+    {
+        hp -= dmg;
+    }
+
+    public int GetHP()
+    {
+        return hp;
+    }
 }
