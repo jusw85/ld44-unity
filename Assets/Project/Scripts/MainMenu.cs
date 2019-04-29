0a5ecbc91b2107f33decf73461ac85a0bc6c6d4c
ziczach <czc85@hotmail.com>
2019-04-29 21:11:15 +0800

    Added Title Scene

---
+++ Assets/Project/Scripts/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Main_menu : MonoBehaviour 
+{
+
+	public string startLevel;
+
+	public string levelSelect;
+
+	public void NewGame()
+	{
+		Application.LoadLevel (startLevel);
+	}
+
+	public void LevelSelect()
+	{
+		Application.LoadLevel (levelSelect);
+	}
+
+	public void QuitGame()
+	{
+		Debug.Log ("Game Exited");
+		Application.Quit();
+	}
+
+}
