da2e6fb37cdd13ac1024be64aa28ab1fcd139092
Justin Wong <jusw85@hotmail.com>
2019-05-02 00:59:00 +0800

    Add basic charge attacks

--- Assets/Project/Scripts/MainMenu.cs
+++ Assets/Project/Scripts/MainMenu.cs
@@ -5,21 +5,8 @@ public class MainMenu : MonoBehaviour
 {
     public string startLevel;
 
-    // public string levelSelect;
-
-    public void NewGame()
+    public void StartGame()
     {
         SceneManager.LoadScene(startLevel);
-    }
-
-    // public void LevelSelect()
-    // {
-    // Application.LoadLevel (levelSelect);
-    // }
-
-    public void QuitGame()
-    {
-        Debug.Log("Game Exited");
-        Application.Quit();
     }
 }
--- Assets/Project/Scripts/SkillBarController.cs
+++ Assets/Project/Scripts/SkillBarController.cs
@@ -19,19 +19,27 @@ public class SkillBarController : MonoBehaviour
         public KeyCode keyCode;
     }
 
-    public float globalCooldown;
+//    public float globalCooldown;
     public SkillButton[] skills;
     public PlayerController player;
+    public Slider chargeBar;
 
     [NonSerialized]
     public bool isInputFrozen;
 
-    private void OnValidate()
+    private SkillButton currentButtonPressed;
+
+//    private void OnValidate()
+//    {
+//        foreach (SkillButton skill in skills)
+//        {
+//            skill.cooldown = Math.Max(globalCooldown, skill.cooldown);
+//        }
+//    }
+
+    private void Awake()
     {
-        foreach (SkillButton skill in skills)
-        {
-            skill.cooldown = Math.Max(globalCooldown, skill.cooldown);
-        }
+        currentButtonPressed = null;
     }
 
     private void Start()
@@ -44,53 +52,91 @@ public class SkillBarController : MonoBehaviour
 
     private void Update()
     {
-        bool isButtonPressed = false;
-        if (!isInputFrozen)
-        {
-            foreach (SkillButton s in skills)
-            {
-                if (Input.GetKeyDown(s.keyCode) && s.cooldownImage.fillAmount <= 0 &&
-                    player.GetNumSlaves() >= s.skillCost)
-                {
-                    s.cooldownImage.fillAmount = 1.0f;
-                    s.currentCooldown = s.cooldown;
-                    isButtonPressed = true;
-                    player.HandleSkill(s.key, s.skillCost);
-                }
-            }
-        }
-
-        if (isButtonPressed)
-        {
-            foreach (SkillButton s in skills)
-            {
-                if (player.GetNumSlaves() < s.skillCost)
-                {
-                    s.cooldownImage.fillAmount = 1.0f;
-                    continue;
-                }
-
-                if (s.cooldownImage.fillAmount <= 0)
-                {
-                    s.cooldownImage.fillAmount = 1.0f;
-                    s.currentCooldown = globalCooldown;
-                }
-                else
-                {
-                    var realTimeCooldown = s.cooldownImage.fillAmount * s.currentCooldown;
-                    realTimeCooldown = Math.Max(realTimeCooldown, globalCooldown);
-                    s.cooldownImage.fillAmount = Math.Min(realTimeCooldown / s.currentCooldown, 1);
-                }
-            }
-        }
-
+        SkillButton buttonPress = null;
         foreach (SkillButton s in skills)
         {
-            if (s.cooldownImage.fillAmount > 0 && player.GetNumSlaves() >= s.skillCost)
+            if (Input.GetKey(s.keyCode))
             {
-                s.cooldownImage.fillAmount -= Time.deltaTime / s.currentCooldown;
-                s.cooldownImage.fillAmount = Mathf.Clamp(s.cooldownImage.fillAmount, 0f, 1f);
+                buttonPress = s;
+                break;
+            }
+        }
+
+        if (currentButtonPressed == null)
+        {
+            currentButtonPressed = buttonPress;
+        }
+
+        if (currentButtonPressed != null)
+        {
+            if (currentButtonPressed == buttonPress)
+            {
+                chargeBar.value += Time.deltaTime / currentButtonPressed.cooldown;
+                chargeBar.value = Mathf.Clamp(chargeBar.value, 0f, 1f);
+                if (chargeBar.value >= 1)
+                {
+                    player.HandleSkill(currentButtonPressed.key, currentButtonPressed.skillCost);
+                    chargeBar.value = 0f;
+                    currentButtonPressed = null;
+                }
+            }
+            else
+            {
+                chargeBar.value = 0f;
+                currentButtonPressed = null;
             }
         }
     }
+
+//    private void Update()
+//    {
+//        bool isButtonPressed = false;
+//        if (!isInputFrozen)
+//        {
+//            foreach (SkillButton s in skills)
+//            {
+//                if (Input.GetKeyDown(s.keyCode) && s.cooldownImage.fillAmount <= 0 &&
+//                    player.GetNumSlaves() >= s.skillCost)
+//                {
+//                    s.cooldownImage.fillAmount = 1.0f;
+//                    s.currentCooldown = s.cooldown;
+//                    isButtonPressed = true;
+//                    player.HandleSkill(s.key, s.skillCost);
+//                }
+//            }
+//        }
+//
+//        if (isButtonPressed)
+//        {
+//            foreach (SkillButton s in skills)
+//            {
+//                if (player.GetNumSlaves() < s.skillCost)
+//                {
+//                    s.cooldownImage.fillAmount = 1.0f;
+//                    continue;
+//                }
+//
+//                if (s.cooldownImage.fillAmount <= 0)
+//                {
+//                    s.cooldownImage.fillAmount = 1.0f;
+//                    s.currentCooldown = globalCooldown;
+//                }
+//                else
+//                {
+//                    var realTimeCooldown = s.cooldownImage.fillAmount * s.currentCooldown;
+//                    realTimeCooldown = Math.Max(realTimeCooldown, globalCooldown);
+//                    s.cooldownImage.fillAmount = Math.Min(realTimeCooldown / s.currentCooldown, 1);
+//                }
+//            }
+//        }
+//
+//        foreach (SkillButton s in skills)
+//        {
+//            if (s.cooldownImage.fillAmount > 0 && player.GetNumSlaves() >= s.skillCost)
+//            {
+//                s.cooldownImage.fillAmount -= Time.deltaTime / s.currentCooldown;
+//                s.cooldownImage.fillAmount = Mathf.Clamp(s.cooldownImage.fillAmount, 0f, 1f);
+//            }
+//        }
+//    }
 }
